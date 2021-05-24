26e479940973cdf1157d98b31035af7274dc6910
Denis Sobolev <denis.sobolev@hyperion-tech.co>
2021-05-24 19:47:04 +0300

    Emails

---
+++ www/resources/views/email/contact.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Email</title>
+</head>
+<body>
+    <h1>The topic is: {{ $topic }}</h1>
+    <p>Some Text Here</p>
+</body>
+</html>
---
+++ www/resources/views/email/md-contact.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# The mail is about
+
+Some text here
+
+- list item
+- and again
+- and here
+
+@component('mail::button', ['url' => 'https://example.com'])
+Example Com
+@endcomponent
+
+
+@endcomponent
