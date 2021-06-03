dfcfce83dc7086af9467742fa6b158f12a933a02
Denis Sobolev <denis.sobolev@hyperion-tech.co>
2021-06-03 21:01:33 +0300

    Roles and permisssions - migration

---
+++ www/database/migrations/2021_06_03_135753_create_roles_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('label')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('label')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('permisson_id');
+            $table->timestamps();
+
+            $table->primary(['role_id', 'permisson_id']);
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+
+            $table->foreign('permisson_id')
+                ->references('id')
+                ->on('permissons')
+                ->onDelete('cascade');        
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+        });
+    }
+}
