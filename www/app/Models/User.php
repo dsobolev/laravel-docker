b3d5b3505ec55ea26669d2775099aee0aaa36cb7
Denis Sobolev <denis.sobolev@hyperion-tech.co>
2021-07-19 19:44:32 +0300

    Added User::getPermissionsLabels method

--- www/app/Models/User.php
+++ www/app/Models/User.php
@@ -58,6 +58,21 @@ class User extends Authenticatable
     }
 
     /**
+     * @return array List of permission labels - for HTML
+     */
+    public function getPermissionsLabels()
+    {
+        return is_null($this->role)
+            ? []
+            : $this->role->permissions->pluck('label')->all();
+    }
+
+    public function permittedTo($permission)
+    {
+        return $this->getPermissionsNames()->contains($permission);
+    }
+
+    /**
      * @return Illuminate\Support\Collection
      *
      * Returns empty array if role hasn't been assigned yet
@@ -67,10 +82,5 @@ class User extends Authenticatable
         return is_null($this->role)
             ? collect([])
             : $this->role->permissions->pluck('name');
-    }
-
-    public function permittedTo($permission)
-    {
-        return $this->getPermissionsNames()->contains($permission);
     }
 }
