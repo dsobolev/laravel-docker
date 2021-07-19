3012debc95a01f7ebb8c0fe7b3a84287f9eb4729
Denis Sobolev <denis.sobolev@hyperion-tech.co>
2021-07-19 18:31:49 +0300

    All notifications view

---
+++ www/resources/views/users/notifications.blade.php
@@ -0,0 +1,25 @@
+@extends ('layout')
+
+@section ('content')
+<h1>Notifications</h1>
+<div class="container">        
+    <ul>
+        @forelse ($notifications as $item)
+            @if ( 'App\Notifications\PaymentReceived' === $item->type )
+                <li>
+                    Successful payment at {{ $item->created_at }}
+                    @if ( ! empty($item->data['amount']) )
+                        <span>(${{ $item->data['amount'] }})</span>
+                    @endif
+                </li>
+            @elseif ( 'App\Notifications\UserRegistered' === $item->type )
+                <li>
+                    New user registered
+                </li>
+            @endif
+        @empty
+            <p>No new notifications received yet...</p>
+        @endforelse
+    </ul>
+</div>
+@endsection
