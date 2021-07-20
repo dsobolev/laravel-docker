6e9a17c0fbe80a8f4df4af24dbd8bfe93faf7e46
Denis Sobolev <denis.sobolev@hyperion-tech.co>
2021-07-20 19:20:15 +0300

    Delete odds from UserPolicy

--- www/app/Policies/UserPolicy.php
+++ www/app/Policies/UserPolicy.php
@@ -21,18 +21,6 @@ class UserPolicy
     }
 
     /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $User
-     * @return mixed
-     */
-    public function view(User $user, User $User)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
@@ -41,41 +29,5 @@ class UserPolicy
     public function update(User $user)
     {
         return $user->permittedTo('edit_users');
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $User
-     * @return mixed
-     */
-    public function delete(User $user, User $User)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $User
-     * @return mixed
-     */
-    public function restore(User $user, User $User)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\=User  $User
-     * @return mixed
-     */
-    public function forceDelete(User $user, User $User)
-    {
-        //
     }
 }
